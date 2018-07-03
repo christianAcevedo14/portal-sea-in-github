16c9fb7c069426f2952c62f42324f5842f3e2dfe
Francisco J Colón Rodríguez <fjcr1018@gmail.com>
2018-07-03 11:56:46 -0400

    Modificaciones e integración de modelos creados al modelo de Usuarios.

--- app/User.php
+++ app/User.php
@@ -2,28 +2,23 @@
 
 namespace App;
 
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable, SoftDeletes;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
+    protected $guarded = [];
+    protected $hidden = ['password', 'remember_token'];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
+    function programmatic_unit()
+    {
+        return $this->belongsTo(ProgrammaticUnit::class);
+    }
+    function title()
+    {
+        return $this->belongsTo(Title::class);
+    }
 }
---
+++ database/migrations/2018_07_03_300000_create_users_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('title_id')->unsigned();
+            $table->integer('programmatic_unit_id')->unsigned();
+            $table->string('first_name');
+            $table->string('surname');
+            $table->string('second_surname');
+            $table->string('name');
+            $table->string('phone')->nullable();
+            $table->string('email')->unique();
+            $table->string('password')->default(Hash::make('pagri'));
+            $table->rememberToken();
+            $table->softDeletes();
+            $table->timestamps();
+
+            $table->foreign('title_id')->references('id')->on('titles');
+            $table->foreign('programmatic_unit_id')->references('id')->on('programmatic_units');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
