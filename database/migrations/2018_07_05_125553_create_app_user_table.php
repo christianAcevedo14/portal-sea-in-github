a42e0012fa94e2a2493f0f79e1d55d0f3478ce73
Francisco J Colón Rodríguez <fjcr1018@gmail.com>
2018-07-05 09:25:55 -0400

    Cambio de nombres a migraciones existentes.

---
+++ database/migrations/2018_07_05_125553_create_app_user_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAppUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('app_user', function (Blueprint $table) {
+            $table->integer('app_id')->unsigned();
+            $table->integer('user_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('app_id')->references('id')->on('apps');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('app_user');
+    }
+}
---
+++ database/seeds/AppsTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\App;
+use Illuminate\Database\Seeder;
+
+class AppsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $regions = [
+            ['name' => 'SISE', 'url' => 'sise'],
+            ['name' => 'pagriWeb', 'url' => 'pagriweb'],
+            ['name' => '4-H', 'url' => '4h'],
+            ['name' => 'CFC', 'url' => 'cfc'],
+            ['name' => 'DRC', 'url' => 'drc'],
+        ];
+
+        foreach ($regions as $region)
+        {
+            App::create($region);
+        }
+    }
+}
