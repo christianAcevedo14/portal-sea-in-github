b7a0e67ecdaa3aac35ec762482513d8c9f936fb4
ramonfnieves <Barseesfab11>
2018-07-23 22:58:38 -0400

    Dinamic Select CriticalIssues, Materias y Objetivos

---
+++ public/js/report/report.js
@@ -0,0 +1,35 @@
+
+$(function () {
+    $('#select-critical_issue').on('change', onSelectCriticalIssueChange);
+});
+
+
+
+function onSelectCriticalIssueChange() {
+    var critical_issue_id  = $(this).val();
+
+    if(!critical_issue_id)
+    {
+        $('#select-materia').html('<option value="">Seleccione Materia</option>');
+        return;
+    }
+
+    $.get('/sise/issue/'+critical_issue_id+'/materias', function (data) {
+        var html_select = '<option value="">Seleccione Materia</option>';
+        for (var i = 0; i < data.length; ++i){
+            html_select += '<option value="'+data[i].code+'">'+data[i].description+'</option>\n'
+            $('#select-materia').html(html_select);
+
+        }
+
+            });
+
+   /* $.get('/api/issue/'+critical_issue_id+'/materias', function (data) {
+        var html_select = '<option value="">Seleccione Materia</option>';
+        for (var i = 0; i < data.length; ++i)
+            html_select+='<option value="+data[i].id+">'+data[i].name+'</option>';
+        console.log(html_select);l
+        $('#select-materia').html(html_select);
+    });*/
+}
+
