16232bbd6b55dc66f1a55492d94d5c6b5a24e8dc
Ramon Nieves <ramonf.nieves@upr.edu>
2018-07-27 11:45:58 -0400

    Dynamic Select Demo

--- public/js/report/report.js
+++ public/js/report/report.js
@@ -1,11 +1,45 @@
-
 $(function () {
     $('#select-critical_issue').on('change', onSelectCriticalIssueChange);
+    $('#select-materia').on('change', onSelectMateriaChange);
 });
 
 
-
 function onSelectCriticalIssueChange() {
+    console.log('Funciona Materia')
+    var critical_issue_id = $(this).val();
+    //console.log(critical_issue_id);
+
+    $.get('/sise/issue/' + critical_issue_id + '/materias', function (data) {
+
+        // for testing purpusos
+        console.log(data);
+        console.log(data.length);
+
+        var html_select = '<option value="">Seleccione Materia</option>';
+        for (var i = 0; i < data.length; ++i) {
+            html_select += '<option value="' + data[i].code + '">' + data[i].description + '</option>';
+            $('#select-materia').html(html_select);
+
+        }
+    });
+
+
+}
+
+function onSelectMateriaChange() {
+    console.log('Funciona Objetivo')
+    var materia_id = $(this).val();
+
+    $.get('/sise/materia/' + materia_id + '/objetivos', function (data) {
+
+        console.log(data);
+    });
+
+
+}
+
+
+/*function onSelectCriticalIssueChange() {
     var critical_issue_id  = $(this).val();
 
     if(!critical_issue_id)
@@ -23,5 +57,5 @@ function onSelectCriticalIssueChange() {
         }
 
             });
-}
+}*/
 
