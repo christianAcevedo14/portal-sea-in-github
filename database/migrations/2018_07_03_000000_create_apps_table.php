ec83b63a67d3751d746a91b17c33e9b077f5aeaf
Francisco J Colón Rodríguez <fjcr1018@gmail.com>
2018-08-13 10:10:02 -0400

    Añadida columna para guardar icono de aplicación.

--- database/migrations/2018_07_03_000000_create_apps_table.php
+++ database/migrations/2018_07_03_000000_create_apps_table.php
@@ -17,6 +17,7 @@ class CreateAppsTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('url');
+            $table->string('icon');
             $table->timestamps();
         });
     }
