a8016757c25b019aae676a651e0dfbbaf6c1dfa8
ramonfnieves <Barseesfab11>
2018-09-23 21:17:35 -0400

    Icon para los apps

--- database/seeds/AppsTableSeeder.php
+++ database/seeds/AppsTableSeeder.php
@@ -16,8 +16,8 @@ class AppsTableSeeder extends Seeder
             ['name' => 'pagriWeb', 'url' => 'pagri', 'icon' => 'fa fa-tree'],
             ['name' => 'SISE', 'url' => 'sise', 'icon' => 'fa fa-file-text'],
             ['name' => '4-H', 'url' => '4h', 'icon' => 'fa fa-odnoklassniki'],
-            ['name' => 'CFC', 'url' => 'cfc', 'icon' => ''],
-            ['name' => 'DRC', 'url' => 'drc', 'icon' => ''],
+            ['name' => 'CFC', 'url' => 'cfc', 'icon' => 'fa fa-group'],
+            ['name' => 'DRC', 'url' => 'drc', 'icon' => 'fa fa-home'],
             ['name' => 'Usuarios', 'url' => 'users', 'icon' => 'fa fa-user'],
         ];
 
