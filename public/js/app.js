031525547ff48341c320903de9e9109039c9a9f1
Francisco J. Colón Rodríguez <fjcr1018@gmail.com>
2019-01-25 11:44:37 -0400

    Código de prueba para eliminar las materias 170, 180, 190.

--- public/js/app.js
+++ public/js/app.js
@@ -48255,7 +48255,7 @@ exports = module.exports = __webpack_require__(3)(false);
 
 
 // module
-exports.push([module.i, "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n", ""]);
+exports.push([module.i, "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n", ""]);
 
 // exports
 
@@ -48366,8 +48366,38 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
                 });
             });
 
-            if (program.code !== 'CIA1' && program.code !== 'CIC1' && program.code !== 'CIJ1' && program.code !== 'CIR1' && program.code !== 'PPG') {
-                matters = matters.splice(0, matters.length - 3);
+            // Código de pruebas para eliminar las materias 170, 180, 190
+
+            if (program.code !== 'CIA1' && program.code !== 'CIC1' && program.code !== 'CIJ1' && program.code !== 'CIR1') {
+                var recursos_externos = matters.find(function (matter) {
+                    return matter.code === '170';
+                });
+                var index_recursos_externos = matters.indexOf(recursos_externos);
+                matters.splice(index_recursos_externos, 1);
+
+                var evaluacion = matters.find(function (matter) {
+                    return matter.code === '180';
+                });
+                var index_evaluacion = matters.indexOf(evaluacion);
+                matters.splice(index_evaluacion, 1);
+
+                var tecnologia = matters.find(function (matter) {
+                    return matter.code === '190';
+                });
+                var index_tecnologia = matters.indexOf(tecnologia);
+                matters.splice(index_tecnologia, 1);
+            } else if (program.code === 'CIC2' && program.code === 'CIC4') {
+                var _recursos_externos = matters.find(function (matter) {
+                    return matter.code === '170';
+                });
+                var _index_recursos_externos = matters.indexOf(_recursos_externos);
+                matters.splice(_index_recursos_externos, 1);
+
+                var _tecnologia = matters.find(function (matter) {
+                    return matter.code === '190';
+                });
+                var _index_tecnologia = matters.indexOf(_tecnologia);
+                matters.splice(_index_tecnologia, 1);
             }
 
             this.form_elements.matters.push(matters);
