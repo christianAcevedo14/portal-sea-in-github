ea7774538dc26ba42456d8b019fae1e0e9781800
Ramon <ramonf.nieves@upr.edu>
2019-02-15 15:12:00 -0400

    Nuevo intento para 180 en C2 y C4

--- public/js/app.js
+++ public/js/app.js
@@ -49078,7 +49078,7 @@ exports = module.exports = __webpack_require__(3)(false);
 
 
 // module
-exports.push([module.i, "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n", ""]);
+exports.push([module.i, "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n", ""]);
 
 // exports
 
@@ -49089,6 +49089,12 @@ exports.push([module.i, "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\
 
 "use strict";
 Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+//
+//
+//
+//
+//
+//
 //
 //
 //
@@ -49191,34 +49197,50 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 
             // Código de pruebas para eliminar las materias 170, 180, 190
 
-            if (program.code === 'CIA1' && program.code === 'CIC1' && program.code === 'CIJ1' && program.code === 'CIR1') {
+            if (program.code !== 'CIA1' && program.code !== 'CIC1' && program.code !== 'CIJ1' && program.code !== 'CIR1') {
 
-                var recursos_externos = matters.find(function (matter) {
-                    return matter.code === '170';
-                });
-                this.form_elements.matters.push(recursos_externos);
+                if (program.code === 'CIC2' && program.code === 'CIC4') {
 
-                /*let recursos_externos = matters.find(matter => matter.code === '170');
-                let index_recursos_externos = matters.indexOf(recursos_externos);
-                matters.splice(index_recursos_externos, 1);
-                 let evaluacion = matters.find(matter => matter.code === '180');
-                let index_evaluacion = matters.indexOf(evaluacion);
-                matters.splice(index_evaluacion, 1);
-                 let tecnologia = matters.find(matter => matter.code === '190');
-                let index_tecnologia = matters.indexOf(tecnologia);
-                matters.splice(index_tecnologia, 1);*/
-            } /*else if (program.code !== 'CIC2' && program.code !== 'CIC4') {
-                  let evaluacion = matters.find(matter => matter.code === '180');
-                let index_evaluacion = matters.indexOf(evaluacion);
-                matters.splice(index_evaluacion, 1);
-                  let recursos_externos = matters.find(matter => matter.code === '170');
+                    var recursos_externos = matters.find(function (matter) {
+                        return matter.code === '170';
+                    });
+                    var index_recursos_externos = matters.indexOf(recursos_externos);
+                    matters.splice(index_recursos_externos, 1);
+
+                    var tecnologia = matters.find(function (matter) {
+                        return matter.code === '190';
+                    });
+                    var index_tecnologia = matters.indexOf(tecnologia);
+                    matters.splice(index_tecnologia, 1);
+                } else {
+
+                    var _recursos_externos = matters.find(function (matter) {
+                        return matter.code === '170';
+                    });
+                    var _index_recursos_externos = matters.indexOf(_recursos_externos);
+                    matters.splice(_index_recursos_externos, 1);
+
+                    var evaluacion = matters.find(function (matter) {
+                        return matter.code === '180';
+                    });
+                    var index_evaluacion = matters.indexOf(evaluacion);
+                    matters.splice(index_evaluacion, 1);
+
+                    var _tecnologia = matters.find(function (matter) {
+                        return matter.code === '190';
+                    });
+                    var _index_tecnologia = matters.indexOf(_tecnologia);
+                    matters.splice(_index_tecnologia, 1);
+                }
+            } /*else if (program.code === 'CIC2' && program.code === 'CIC4') {
+                 let recursos_externos = matters.find(matter => matter.code === '170');
                 let index_recursos_externos = matters.indexOf(recursos_externos);
                 matters.splice(index_recursos_externos, 1);
                  let tecnologia = matters.find(matter => matter.code === '190');
                 let index_tecnologia = matters.indexOf(tecnologia);
                 matters.splice(index_tecnologia, 1);
-              }*/
-
+              }
+              */
             this.form_elements.matters.push(matters);
             this.form_elements.enterprises.push(enterprises);
             this.form_elements.enterprises[0].length > 0 ? this.form_elements.hideStyle.display = 'block' : this.form_elements.hideStyle.display = 'none';
@@ -49318,7 +49340,10 @@ var render = function() {
                   { key: index, domProps: { value: program.id } },
                   [
                     _vm._v(
-                      _vm._s(program.code) + " - " + _vm._s(program.description)
+                      _vm._s(program.code) +
+                        " - " +
+                        _vm._s(program.description) +
+                        "\n                    "
                     )
                   ]
                 )
