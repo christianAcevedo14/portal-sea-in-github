a4725799a659f5d396619636a862588901a97ea8
Ramon Nieves <ramonf.nieves@upr.edu>
2019-04-01 15:46:48 -0400

    Se añade nuevo proposito exitosamente.

--- public/js/Cfc/purpose.js
+++ public/js/Cfc/purpose.js
@@ -11,6 +11,10 @@ function addPurposeModal() {
 
 function changeBox() {
     var option_value = document.getElementById("purpose_id").value;
+
+/*    var purpose_description = $(this).parent().prev().text();
+    $('#purpose_description').val(purpose_description);
+    */
     if (option_value == "proposito") {
         $('#modalAddPurpose').modal('show');
     }
