2949ea6c541da75812469f3f3e232f84f786ebab
Erick Matos <erick.matos@upr.edu>
2019-07-03 09:42:08 -0400

    Updated navbar icon for portal-sea.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,14 +12,15 @@
     <meta name="mobile-web-app-capable" content="yes">
     <meta name="HandheldFriendly" content="True">
     <meta name="MobileOptimized" content="320">
-    <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon"/>
-    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('favicon.ico') }}" />
-    <!-- Generated: 2018-04-16 09:29:05 +0200 -->
+{{--    <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon"/>--}}
+{{--    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('favicon.ico') }}" />--}}
+{{--    <!-- Generated: 2018-04-16 09:29:05 +0200 -->--}}
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>Portal del Extensionista | Servicio de Extensión Agrícola</title>
+    <link rel="icon" href="/demo/brand/portal_icon.png" type="image"/>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,300i,400,400i,500,500i,600,600i,700,700i&amp;subset=latin-ext">
     <!-- DataTables -->
