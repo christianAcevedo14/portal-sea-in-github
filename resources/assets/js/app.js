4b5d2ae01b238b5a680c78990e9c3d9cb9a867e3
jrivera95 <jorge.rivera30@upr.edu>
2020-03-27 14:55:39 -0400

    Fixed component route for objectives vue component.

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -58,7 +58,7 @@ Vue.component('pagination', require('laravel-vue-pagination'));
 const routes = [
     { path: '/sise/dashboard', component: Dashboard },
     { path: '/sise/program', component: Program },
-    { path: '/sise/audience', component: Objective },
+    { path: '/sise/objective', component: Objective },
     { path: '/sise/*', component: Dashboard },
     { path: '/*', component: Dashboard },
 ];
