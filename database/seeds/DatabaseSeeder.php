5b2996872cf0408036483ca171f827b2d86fb004
Jorge Rivera <jorge.rivera30@upr.edu>
2020-07-07 16:15:23 -0400

    In DatabaseSeeder.php, commented seeders not related to tutorials to simplify implementing the tutorials feature in the server.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,14 +11,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         $this->call(AppsTableSeeder::class);
+         /*$this->call(AppsTableSeeder::class);
          $this->call(TitlesTableSeeder::class);
          $this->call(RegionsTableSeeder::class);
          $this->call(ProgrammaticUnitsTableSeeder::class);
          $this->call(CitiesTableSeeder::class);
          $this->call(ZipcodesTableSeeder::class);
          $this->call(UsersTableSeeder::class);
-         $this->call(ProgrammaticAreasTableSeeder::class);
+         $this->call(ProgrammaticAreasTableSeeder::class);*/
          $this->call(TutorialAppsTableSeeder::class);
          $this->call(TutorialSectionsTableSeeder::class);
 //         $this->call(OldUsersTableSeeder::class);
