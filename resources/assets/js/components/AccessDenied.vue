2256a152528ffadff3c26b4d7215a303c9cdcdc8
rafaelsoto98 <rafael.soto6@upr.edu>
2020-07-14 16:45:18 -0400

    Created logic to prevent users that are not supervisors to access supervision pages, A gif will be displayed and a button to return to the main menu. Added a query where isCoord will NOT be able to supervise users with titles 14 and 16. Created new component called AccessDenied.vue

---
+++ resources/assets/js/components/AccessDenied.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <img src="/img/hand_stop.gif" class="mb-3" alt="warning image">
+                <div class="text-center">
+                    <h3 class="my-5">Usted no tiene permiso para ver esta página.</h3>
+                    <a :href="/home/" class="btn btn-primary btn-lg text-white"> Menú Principal</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+
+        }
+    }
+</script>
