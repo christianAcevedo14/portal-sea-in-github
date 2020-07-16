22861f241badd126bc05dc78d34d81ffc3a210db
Erick Matos <erick.matos@upr.edu>
2020-07-16 15:00:51 -0400

    Made adjustment so that the "crear informes" button only appears after the plan status is 1 (pendiente a someter) or higher on InformesIndex and UnplannedInformes components. Added the access denied page to Informes and UnplannedInformesCreate so it prevents the user from using the components if the plan status doesn't meet the requirement (1 or higher). Adjusted FPPC controler so that Admins can have access to FPPC. Made the AccessDenied component dynamic and can now show custom messages and url redirects according to it's source.

--- resources/assets/js/components/AccessDenied.vue
+++ resources/assets/js/components/AccessDenied.vue
@@ -1,11 +1,12 @@
 <template>
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-8">
+            <div class="col-md-8 text-center">
+                <h1>¡Alto!</h1>
                 <img src="/img/hand_stop.gif" class="mb-3" alt="warning image">
                 <div class="text-center">
-                    <h3 class="my-5">Usted no tiene permiso para ver esta página.</h3>
-                    <a :href="/home/" class="btn btn-primary btn-lg text-white"> Menú Principal</a>
+                    <h3 class="my-5">{{this.message}}</h3>
+                    <a :href="this.url" class="btn btn-primary btn-lg text-white">{{this.btn}}</a>
                 </div>
             </div>
         </div>
@@ -14,8 +15,26 @@
 
 <script>
     export default {
+        name: "AccessDenied",
+        props: ['source'],
+        data() {
+            return {
+                message: "",
+                url: '',
+                btn: "",
+            }
+        },
+        methods: {},
         mounted() {
-
+            if (this.source === 'plan_status') {
+                this.message = "El status de su Plan de Trabajo no le permite acceder a esta página aún.";
+                this.url = '/sise/plan';
+                this.btn = "Ver Plan";
+            } else {
+                this.message = "Usted no tiene permiso para acceder a esta página.";
+                this.url = '/home/';
+                this.btn = "Menú Principal";
+            }
         }
     }
 </script>
