b283c2bcfeccdc945ec2a22ac41c819bf4e8c069
Erick Matos <erick.matos@upr.edu>
2020-10-14 16:16:25 -0400

    Fixed notifications reloading the page when removing a notification and changed icon appearance on Notifications.vue.

--- resources/assets/js/components/Notifications.vue
+++ resources/assets/js/components/Notifications.vue
@@ -1,19 +1,16 @@
 <template>
     <div class="dropdown d-xs d-md-block">
         <a class="nav-link icon" data-toggle="dropdown">
-            <i class="fe fe-bell"></i>
-            <span class="pb-4" style="color: red" v-if="notifications.length > 0">{{notifications.length}}</span>
+            <span class="btn btn-pill btn-sm mt-1" :class="{ 'btn-outline-primary' : notifications.length > 0 , 'btn-outline-secondary' : notifications.length === 0 }"> <i class="fe fe-bell"></i>
+            <span class="pb-4" style="color: red" v-if="notifications.length > 0">{{notifications.length}}</span></span>
+
         </a>
         <div v-if="notifications.length > 0" class="dropdown-menu dropdown-menu-right dropdown-menu-arrow">
             <a class="dropdown-item d-flex" href="#" v-for="notification in notifications">
                 <a :style="{ backgroundImage: 'url(' + notification.data.user_avatar + ')' }"
                       class="avatar mr-1 align-self-center"></a>
-                <img src="" alt="">
                 <div class="ml-3">
-                    <span class="message">{{ notification.data.message }}</span>
-
-                <!--</div>-->
-<!--                <div class="col-1">-->
+                    <span class="message pt-1">{{ notification.data.message }}</span>
                     <div class="small text-muted ml-4"><strong>Fecha:</strong> {{ new
                         Date(notification.created_at).toLocaleDateString('es-ES', {month: 'long', day: 'numeric', year:
                         'numeric'}) }}
@@ -22,11 +19,8 @@
                 <div>
                     <button type="button" class="btn close float-right mb-6 ml-3" aria-label="Close"
                             v-on:click="removeNotification(notification.id)">
-                        <!--                    <span aria-hidden="true">&times;</span>-->
                     </button>
                 </div>
-                <!--<span class="pl-9 float-right">
-                                    </span>-->
             </a>
             <div class="dropdown-divider"></div>
             <a class="dropdown-item text-center text-muted-dark" href="#" v-on:click="removeAllNotifications()">Marcar
@@ -50,7 +44,7 @@
             removeNotification(id) {
                 let domain = window.location.protocol + '//' + window.location.hostname;
                 axios.get(`${domain}/notifications/markAsRead/` + id);
-                Fire.$emit('ReloadPage');
+                Fire.$emit('ReloadNotifications');
                 },
 
             removeAllNotifications() {
@@ -63,15 +57,8 @@
 
             loadNotifications(){
                 let domain = window.location.protocol + '//' + window.location.hostname;
-
-                // this.$root.disableInterceptor();
-
                 axios.get(`${domain}/notifications/get`).then(response => {
                     this.notifications = response.data
-
-                    // this.$root.enableInterceptor()
-
-                    // console.log('Interceptor Enabled')
                 });
             }
 
@@ -81,7 +68,7 @@
             this.loadNotifications();
             setInterval(() => this.loadNotifications(), 20000)
 
-            Fire.$on('ReloadPage', () => {
+            Fire.$on('ReloadNotifications', () => {
                 this.loadNotifications();
             })
         }
@@ -95,10 +82,11 @@
         max-height: 300px;
         overflow-y:auto;
         width: 600px;
+        transition: .5s ease;
     }
     .message{
         display:block;
-        width:400px;
+        width:430px;
         word-wrap:break-word;
         white-space: normal;
         margin-left: 1rem;
