2a51622ff26d576c33267a5fe9312a5a28b43fc8
Erick Matos <erick.matos@upr.edu>
2020-10-19 14:15:46 -0400

    Created FiscalYearRepository.php to house the logic that determines the current fiscal year, and added construct function to the controllers to access the repository.

---
+++ app/Http/Repositories/FiscalYearRepository.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Repositories;
+
+
+class FiscalYearRepository
+{
+    public function currentFiscalYear ()
+    {
+        /**
+         * Codigo para calcular el año fiscal corriente
+         */
+        $inputDate = now();
+        $fyEnd = "12/01";
+        $fyStart = "12/02";
+
+        $date = strtotime($inputDate);
+        $inputyear = strftime('%Y', $date);
+
+        $fystartdate = strtotime($fyStart . '/' . $inputyear);
+        $fyenddate = strtotime($fyEnd . '/' . $inputyear);
+
+        if ($date <= $fyenddate) {
+            $fy = intval($inputyear);
+        } else {
+            $fy = intval(intval($inputyear) + 1);
+        }
+
+        /**
+         * Codigo para comparar el año fiscal corriente con el created-at
+         */
+
+        $newStartDate = date('Y-m-d', strtotime('-1year', $fystartdate));
+        $newEndDate = date('Y-m-d', $fyenddate);
+
+        return compact('fy' , 'newStartDate' , 'newEndDate');
+    }
+
+}
